ecf22b14c9f062d99a42fa52779cf6b09a9e10ad
Renato Quintão <renatoqs2@gmail.com>
2020-05-20 12:28:25 -0300

    Atualização: Utilizando somente @JsonIgnore

--- src/main/java/com/renatoqs2/cursomc/dominio/Cidade.java
+++ src/main/java/com/renatoqs2/cursomc/dominio/Cidade.java
@@ -1,8 +1,6 @@
 package com.renatoqs2.cursomc.dominio;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cidade implements Serializable{
@@ -22,7 +18,6 @@ public class Cidade implements Serializable{
 	private Integer id;
 	private String nome;
 
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
